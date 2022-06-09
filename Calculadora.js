6ac71819536a9a70e02051946600b87f0ee87a18
pityochner <pietroochner2014@gmail.com>
2022-06-09 13:45:11 -0300

    LaCalcu

--- Calculadora.js
+++ Calculadora.js
@@ -8,8 +8,8 @@ class Calculadora {
     }
 
     dividir(num1, num2) {
-        num1 == 0  && swal({title: "ERROR", text: "No se pueden dividir numeros por 0", icon: "error", button: "OKAY",});
-        num2 == 0  && swal({title: "ERROR", text: "No se pueden dividir numeros por 0", icon: "error", button: "OKAY",});
+        num1 == 0 && swal({title: "ERROR", text: "No se pueden dividir numeros por 0", icon: "error", button: "OKAY",});
+        num2 == 0 && swal({title: "ERROR", text: "No se pueden dividir numeros por 0", icon: "error", button: "OKAY",});
         return num1 / num2;
     }
 
